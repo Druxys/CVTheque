b538fbb5ce959f518a0174700aec887503efcb07
unknown <niconzeweb@wanadoo.fr>
2019-03-07 09:57:32 +0100

    Niko

--- application/controllers/Resume.php
+++ application/controllers/Resume.php
@@ -28,56 +28,126 @@ class Resume  extends CI_Controller
         }
         else {
         $data['title'] = ucfirst($page); // Capitalize the first letter
-        $config = array(
-            array(
-                'field' => 'firstName',
-                'label' => 'FirstName',
-                'rules' => 'required',
-                'errors' => array(
-                    'required' => 'You must provide a %s.',
+            $config = array(
+                array(
+                    'field' => 'firstName',
+                    'label' => 'FirstName',
+                    'rules' => 'trim|required|min_length[3]|max_length[75]',
+                    'errors' => array(
+                        'trim' => 'Le prénom est invalide.',
+                        'required' => 'Veuillez renseigner un prénom.',
+                        'min_length[3]' => 'Le prénom renseigné doit faire au moins 3 caractères.',
+                        'max_length[75]' => 'Le prénom renseigné ne doit pas faire plus de 75 caractères.',
+                    ),
                 ),
-            ),
-            array(
-                'field' => 'lastName',
-                'label' => 'lastName',
-                'rules' => 'required',
-                'errors' => array(
-                    'required' => 'You must provide a %s.',
+                array(
+                    'field' => 'lastName',
+                    'label' => 'lastName',
+                    'rules' => 'trim|required|min_length[3]|max_length[75]',
+                    'errors' => array(
+                        'trim' => 'Le nom est invalide.',
+                        'required' => 'Veuillez renseigner un nom.',
+                        'min_length[3]' => 'Le nom renseigné doit faire au moins 3 caractères.',
+                        'max_length[75]' => 'Le nom renseigné ne doit pas faire plus de 75 caractères.',
+                    ),
                 ),
-            ),
-            array(
-                'field' => 'nationality',
-                'label' => 'nationality',
-                'rules' => 'required',
-                'errors' => array(
-                    'required' => 'You must provide a %s.',
+                array(
+                    'field' => 'nationality',
+                    'label' => 'nationality',
+                    'rules' => 'trim|required|min_length[3]|max_length[75]',
+                    'errors' => array(
+                        'trim' => 'La nationalité est invalide.',
+                        'required' => 'Veuillez renseigner une nationalité.',
+                        'min_length[3]' => 'La nationalité renseignéé doit faire au moins 3 caractères.',
+                        'max_length[75]' => 'La nationalité renseignée ne doit pas faire plus de 75 caractères.',
+                    ),
                 ),
-            ),
-            array(
-                'field' => 'birthDate',
-                'label' => 'birthDate',
-                'rules' => 'required',
-                'errors' => array(
-                    'required' => 'You must provide a %s.',
+                array(
+                    'field' => 'birthDate',
+                    'label' => 'birthDate',
+                    'rules' => 'trim|required',
+                    'errors' => array(
+                        'trim' => 'La date de naissance est invalide.',
+                        'required' => 'Veuillez renseigner une date de naissance.',
+                    ),
                 ),
-            ),
-            array(
-                'field' => 'application',
-                'label' => 'application',
-                'rules' => 'required',
-                'errors' => array(
-                    'required' => 'You must provide a %s.',
+                array(
+                    'field' => 'application',
+                    'label' => 'application',
+                    'rules' => 'trim|required|min_length[3]|max_length[75]',
+                    'errors' => array(
+                        'trim' => 'Le poste ciblé est invalide.',
+                        'required' => 'Veuillez renseigner un poste ciblé.',
+                        'min_length[3]' => 'Le poste ciblé renseigné doit faire au moins 3 caractères.',
+                        'max_length[75]' => 'Le poste ciblé renseigné ne doit pas faire plus de 75 caractères.',
+                    ),
                 ),
-            ),
-            array(
-                'field' => 'description',
-                'label' => 'description',
-                'rules' => 'required',
-                'errors' => array(
-                    'required' => 'You must provide a %s.',
+                array(
+                    'field' => 'description',
+                    'label' => 'description',
+                    'rules' => 'trim|min_length[3]|max_length[1500]',
+                    'errors' => array(
+                        'trim' => 'La description est invalide.',
+                        'required' => 'Veuillez renseigner une courte description.',
+                        'min_length[3]' => 'La description renseignée doit faire au moins 3 caractères.',
+                        'max_length[75]' => 'La description renseignée ne doit pas faire plus de 75 caractères.',
+                    ),
                 ),
-            ),
-        );
+                array(
+                    'field' => 'address',
+                    'label' => 'addresse',
+                    'rules' => 'trim|required|min_length[3]|max_length[75]',
+                    'errors' => array(
+                        'trim' => 'L\'addresse est invalide.',
+                        'required' => 'Veuillez renseigner une addresse.',
+                        'min_length[3]' => 'L\'addresse renseignée doit faire au moins 3 caractères.',
+                        'max_length[75]' => 'L\'addresse renseignée ne doit pas faire plus de 75 caractères.',
+                    ),
+                ),
+                array(
+                    'field' => 'postCode',
+                    'label' => 'code postal',
+                    'rules' => 'trim|required|min_length[3]|max_length[75]',
+                    'errors' => array(
+                        'trim' => 'Le code postal est invalide.',
+                        'required' => 'Veuillez renseigner un code postal.',
+                        'min_length[3]' => 'Le code postal renseigné doit faire au moins 3 caractères.',
+                        'max_length[75]' => 'Le code postal renseigné ne doit pas faire plus de 75 caractères.',
+                    ),
+                ),
+                array(
+                    'field' => 'city',
+                    'label' => 'ville',
+                    'rules' => 'trim|required|min_length[3]|max_length[75]',
+                    'errors' => array(
+                        'trim' => 'La ville renseignée est invalide.',
+                        'required' => 'Veuillez renseigner une ville.',
+                        'min_length[3]' => 'La ville renseignée doit faire au moins 3 caractères.',
+                        'max_length[75]' => 'La ville renseignée ne doit pas faire plus de 75 caractères.',
+                    ),
+                ),
+                array(
+                    'field' => 'mail',
+                    'label' => 'email',
+                    'rules' => 'trim|valid_email|required',
+                    'errors' => array(
+                        'trim' => 'L\'addresse mail renseignée est invalide.',
+                        'valid_email' => 'L\addresse mail renseignée est invalide',
+                        'required' => 'Veuillez renseigner une addresse mail.',
+                    ),
+                ),
+                array(
+                    'field' => 'tel',
+                    'label' => 'telephone',
+                    'rules' => 'trim|required|numeric|exact_length[10]',
+                    'errors' => array(
+                        'trim' => 'Le numéro de téléphone renseigné est invalide.',
+                        'required' => 'Veuillez renseigner un numéro de téléphone.',
+                        'numeric' => 'Le numéro de téléphone doit être composé de caractères numériques.',
+                        'exact_length[10]' => 'Le numéro de téléphone doit faire 10 caractères.'
+                    ),
+                ),
+            );
 
         $this->form_validation->set_rules($config);
 

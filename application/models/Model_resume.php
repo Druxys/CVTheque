5e3158efdb47004e504267d00d0b5100dca321d4
mathflk <math.delp76@gmail.com>
2019-03-07 14:18:00 +0100

    frmQ

--- application/models/Model_resume.php
+++ application/models/Model_resume.php
@@ -65,10 +65,6 @@ class Model_resume extends CI_Model
        // }
     }
 
-
-
-    function insertCertif($atitle,$adate)
-
     function replace1($id ,$genre, $firstName, $lastName, $nationality, $birthDate, $application, $description ,$address, $postCode, $city, $mail, $tel , $idtemplatecvuser){
         $data = array(
             "cvt_users_idcvt_users" => $id,
